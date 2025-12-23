ce41df1aa71c4ee793c659389bffddc1a1199428
userkey <5314175+userkey@user.noreply.gitee.com>
2025-12-23 11:08:28 +0800

    优化小游戏失败判断的流程

--- whimbox/task/minigame_task/minigame_task.py
+++ whimbox/task/minigame_task/minigame_task.py
@@ -1,7 +1,6 @@
-# 穿梭大冒险
+# 小游戏模板
 
-from unittest import skip
-from whimbox.task.task_template import TaskTemplate, register_step
+from whimbox.task.task_template import *
 from whimbox.ui.ui_assets import *
 from whimbox.interaction.interaction_core import itt
 from whimbox.common.keybind import keybind
@@ -14,7 +13,7 @@ from whimbox.common.scripts_manager import *
 class MinigameTask(TaskTemplate):
     def __init__(self, macro_name: str):
         super().__init__("minigame_task")
-        self.max_retry_times = 2
+        self.max_retry_times = 1
         self.retry_times = 0
         self.macro_name = macro_name
 
@@ -49,30 +48,52 @@ class MinigameTask(TaskTemplate):
     
     @ register_step("检查是否失败")
     def step3(self):
-        if itt.get_img_existence(ButtonMinigameRetry):
-            if self.retry_times > self.max_retry_times:
+        itt.delay(1, comment="等待小游戏结束")
+        if itt.get_img_existence(IconPageMainFeature):
+            # 如果还能看到主界面标志，说明小游戏没结束，也视作失败，手动重试
+            itt.key_press("esc")
+            itt.delay(0.5, comment="等待esc菜单显示")
+            if self.retry_times >= self.max_retry_times:
+                self.log_to_gui(f"小游戏已重试{self.max_retry_times}次，依旧失败，只能退出了")
+                scroll_find_click(AreaMinigameEscSelect, "退出", str_match_mode=1)
+                wait_until_appear_then_click(ButtonMinigameRetryOk)
+                wait_until_appear_then_click(ButtonMinigameQuit, retry_time=5)
+                self.update_task_result(status=STATE_TYPE_STOP, message="小游戏失败了")
+                return 'step4'
+            else:
+                self.retry_times += 1
+                self.log_to_gui(f"小游戏失败了，再试一遍")
+                scroll_find_click(AreaMinigameEscSelect, "重试", str_match_mode=1)
+                wait_until_appear_then_click(ButtonMinigameRetryOk)
+                return 'step2'
+            
+        elif itt.get_img_existence(ButtonMinigameRetry):
+            # 出现重试按钮，说明小游戏失败了
+            if self.retry_times >= self.max_retry_times:
                 self.log_to_gui(f"小游戏已重试{self.max_retry_times}次，依旧失败，只能退出了")
                 wait_until_appear_then_click(ButtonMinigameQuit)
+                self.update_task_result(status=STATE_TYPE_STOP, message="小游戏失败了")
                 return 'step4'
             else:
                 self.retry_times += 1
                 self.log_to_gui(f"小游戏失败了，再试一遍")
                 itt.appear_then_click(ButtonMinigameRetry)
-                time.sleep(0.5)
                 wait_until_appear_then_click(ButtonMinigameRetryOk)
                 return 'step2'
+
         else:
             self.log_to_gui("小游戏成功了")
             return 'step4'
 
     @ register_step("结束小游戏")
     def step4(self):
-        wait_until_appear(IconSkipDialog, 10)
+        # 如果出现主界面标志，就不用对话直接结束
+        if wait_until_appear(IconPageMainFeature, retry_time=5):
+            return
+        wait_until_appear(IconSkipDialog, retry_time=10)
         skip_dialog()
         if skip_get_award():
             self.update_task_result(message="成功领取小游戏奖励")
-        else:
-            raise Exception("领取小游戏奖励失败")
         skip_dialog()
         if not scroll_find_click(AreaDialogSelection, "谢谢你", str_match_mode=1):
             raise Exception("未找到对话选项：不了")
